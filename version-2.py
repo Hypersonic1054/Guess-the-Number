417f4b021a344f9dcf48773990e117995af4de38
Hyper <65469240+Hypersonic1054@users.noreply.github.com>
2025-10-29 12:45:42 -0400

    Create version-2.py

---
+++ version-2.py
@@ -0,0 +1,17 @@
+import random as rd
+
+magic_number = rd.randint(1, 10)
+#3print(magic_number)
+
+guess = int(input("Guess a number between 1 and 10: "))
+
+while guess != magic_number:
+  print("You lose!")
+  guess = int(input("Guess a number between 1 and 10: "))
+else: 
+  print("You win!")
+
+
+  
+  
+  
