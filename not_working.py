8eea2b64560886f435983179661871a11096db86
Hyper <65469240+Hypersonic1054@users.noreply.github.com>
2025-11-10 10:48:50 -0500

    Create not_working.py

---
+++ not_working.py
@@ -0,0 +1,54 @@
+import random as rd
+import os
+
+play = True
+conditions = ["win", "lose"]
+games_played = 0
+games_won = 0
+
+while play:
+  guess_list = []
+  counter = 4
+  magic_number = rd.randint(1, 100)
+  print(magic_number)
+  guess = int(input("Guess a number between 1 and 100: "))
+  guess_list.append(guess)
+    
+
+  while guess != magic_number and counter > 0:
+    counter-= 1
+    
+    if guess > magic_number:
+      print("too high")
+    elif guess < magic_number:
+      print("too low")
+      
+      print("Try again!")
+      print("%s guesses remain." %(counter))
+      guess = int(input("Guess a number between 1 and 100: "))
+      guess_list.append(guess)
+      print ("You have already guessed :%s" %guess_list)
+    
+    if counter == 0:
+      print ("You lose!")
+      games_played += 1
+    else: 
+      print("You win!")
+      games_won += 1
+      games_played += 1
+
+    print("SCOREBOARD")
+    print("----------")
+    print("Games Won: %s" % (games_won))
+    print("Games Played: %s" % (games_played))      
+        
+      
+    
+      
+  answer = input("Would you like to play again? (y/n)")
+  os.system("clear")
+  if answer == "y":
+    play + True
+  elif answer == "n":
+    play = False
+  
